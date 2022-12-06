9b54ac16ef58cfbe6c13ea93ab95c5cd6fa7ebcf
Oskar30 <85636221+Oskar30@users.noreply.github.com>
2022-12-06 20:50:28 +0400

    sort

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
+    id = models.BigAutoField(auto_created=False, primary_key=True)
     name = models.CharField(max_length=50)
-    image = models.ImageField()
-    price = models.IntegerField()
-    release_date = models.DateField()
-    lte_exists = models.BooleanField()
+    image = models.ImageField(default=None)
+    price = models.IntegerField(default=None)
+    release_date = models.DateField(default=None)
+    lte_exists = models.BooleanField(default=None)
+    slug = models.CharField(max_length=50, default=None)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,53 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    
+    sort = request.GET.get('sort')
+
+    if sort == 'name':
+        phone_objects = Phone.objects.order_by('name')
+
+    elif sort == 'min_price':
+        phone_objects = Phone.objects.order_by('price')
+
+    elif sort == 'max_price':
+        phone_objects = Phone.objects.order_by('-price')
+
+    else:
+        phone_objects = Phone.objects.all()
+
+
+    phones = {'phones':[]}
+
+    for phone in phone_objects:
+        new_phone = {
+            'id':phone.id,
+            'name':phone.name,            
+            'image':phone.image,
+            'price':phone.price,
+            'release_date':phone.release_date,
+            'lte_exists':phone.lte_exists,
+            'slug':phone.slug
+            }
+        phones['phones'].append(new_phone)
+    
+    context = phones
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_objects = Phone.objects.filter(slug=f'{slug}')
+
+    for phone in phone_objects:
+        context = {'phone':{
+            'id':phone.id,
+            'name':phone.name,            
+            'image':phone.image,
+            'price':phone.price,
+            'release_date':phone.release_date,
+            'lte_exists':phone.lte_exists,
+            'slug':phone.slug}
+            }
+
     return render(request, template, context)
