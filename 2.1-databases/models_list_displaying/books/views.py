1d9ebe0431f7852c700990f86b74c2e0f59de860
Oskar30 <85636221+Oskar30@users.noreply.github.com>
2022-12-14 06:55:57 +0400

    re

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
-    return render(request, template, context)
+    book_objects = Book.objects.all()
 
+    books = {'books':[]}
+
+    for book in book_objects:
+        new_book = {
+            'name':book.name,
+            'author':book.author,
+            'pub_date':book.pub_date 
+            }
+        books['books'].append(new_book)
+    
+    context = books
+    return render(request, template, context)
