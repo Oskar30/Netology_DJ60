eeae6eca0ed3b39de98dfb7b661eac3b33a1cef8
Oskar30 <85636221+Oskar30@users.noreply.github.com>
2022-12-29 08:07:33 +0400

    sc

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, Tag, Scope
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    articles_objects = Article.objects.all().order_by('-published_at')
+
+    context = {'object_list':articles_objects}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student, Teacher, TeachersforStudent
+from .models import Student, TeachersforStudent
 
 
 def students_list(request):
