bb66517e936be857966f9bbdd72a507997ad09f1
Oskar30 <85636221+Oskar30@users.noreply.github.com>
2022-12-31 11:28:05 +0400

    add

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):    
+    name = models.CharField(max_length=30)
+
+
+class Measurement(models.Model):
+    # id
+    temp = models.FloatField
+    time = models.DateTimeField
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from measurement.views import demo
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('', demo),
 ]
